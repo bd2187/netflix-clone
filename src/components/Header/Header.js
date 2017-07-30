5d87e4f9ca8d33ef153b2726f9cb758b1b905691
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-07-30 10:59:07 -0700

    Add NavLinks to Navbar.Collapse in Header Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.scss';
 
 function Header () {
+  const netflixRed = "#B9090B";
   return (
     <Navbar inverse collapseOnSelect fixedTop>
       <Navbar.Header>
         <Navbar.Brand>
-          <a href="/">Netflix</a>
+          <Link to="/">Netflix</Link>
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar.Header>
+
+
+
       <Navbar.Collapse>
-        <Nav>
-          <NavItem eventKey={1} href="#">Home</NavItem>
-          <NavItem eventKey={2} href="#">Action & Adventure</NavItem>
-          <NavItem eventKey={2} href="#">Comedies</NavItem>
-          <NavItem eventKey={2} href="#">Documentaries</NavItem>
-          <NavItem eventKey={2} href="#">Dramas</NavItem>
-          <NavItem eventKey={2} href="#">Horror Movies</NavItem>
-          <NavItem eventKey={2} href="#">Romantic Movies</NavItem>
-          <NavItem eventKey={2} href="#">Sci-Fi & Fantasy</NavItem>
-          <NavItem eventKey={2} href="#">Thrillers</NavItem>
-        </Nav>
+        <ul>
+          <li>
+            <NavLink exact to="/" activeStyle={{color: netflixRed}}>
+              Home
+            </NavLink>
+            <NavLink to="/genre/Comedies" activeStyle={{color: netflixRed}}>
+              Comedies
+            </NavLink>
+            <NavLink to="/genre/Documentaries" activeStyle={{color: netflixRed}}>
+              Documentaries
+            </NavLink>
+            <NavLink to="/genre/Dramas" activeStyle={{color: netflixRed}}>
+              Dramas
+            </NavLink>
+            <NavLink to="/genre/Horror" activeStyle={{color: netflixRed}}>
+              Horror
+            </NavLink>
+            <NavLink to="/genre/Romance" activeStyle={{color: netflixRed}}>
+              Romance
+            </NavLink>
+            <NavLink to="/genre/Sci-Fi" activeStyle={{color: netflixRed}}>
+              Sci-Fi
+            </NavLink>
+            <NavLink to="/genre/Thrillers" activeStyle={{color: netflixRed}}>
+              Thrillers
+            </NavLink>
+          </li>
+        </ul>
         <Nav pullRight>
         </Nav>
       </Navbar.Collapse>
@@ -31,3 +53,24 @@ function Header () {
 }
 
 export default Header;
+
+
+
+// <Navbar.Collapse>
+//   <Nav>
+//     <NavItem eventKey={1} href="/">
+//       <Link to="/">Home</Link>
+//     </NavItem>
+//     <NavItem eventKey={2} href="#">
+//       <Link to="/genre/Comedies">Comedies</Link>
+//     </NavItem>
+//     <NavItem eventKey={2} href="#">Documentaries</NavItem>
+//     <NavItem eventKey={2} href="#">Dramas</NavItem>
+//     <NavItem eventKey={2} href="#">Horror Movies</NavItem>
+//     <NavItem eventKey={2} href="#">Romantic Movies</NavItem>
+//     <NavItem eventKey={2} href="#">Sci-Fi & Fantasy</NavItem>
+//     <NavItem eventKey={2} href="#">Thrillers</NavItem>
+//   </Nav>
+//   <Nav pullRight>
+//   </Nav>
+// </Navbar.Collapse>
