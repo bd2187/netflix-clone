21a510f2549ac7cd20ad44714305c34dffeb6922
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-30 10:27:41 -0700

    Convert Home component to a stateless container. Have Home container render MovieGenre component rather than rendering ActionAdventureContainer. Consider replacing the rest of the genre containers with the mMovieGenre component so we can remove uneccessary containers. Reuse the MovieGenre component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import MainMovieContainer from '../containers/MainMovieContainer';
 import ActionAdventureContainer from '../containers/ActionAdventureContainer';
@@ -10,23 +10,60 @@ import RomanceContainer from '../containers/RomanceContainer';
 import ScifiContainer from '../containers/ScifiContainer';
 import ThrillerContainer from '../containers/ThrillerContainer';
 import TrendingContainer from '../containers/TrendingContainer';
+import MovieGenre from './MovieGenre/MovieGenre';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchActionAdventure } from '../actions';
 
-function Home () {
-  return (
-    <div>
-      <MainMovieContainer />
-      <TrendingContainer />
-      <ActionAdventureContainer />
-      <ComediesContainer />
-      <DocumentariesContainer />
-      <DramasContainer />
-      <HorrorContainer />
-      <RomanceContainer />
-      <ScifiContainer />
-      <ThrillerContainer />
-    </div>
-  )
+// <MovieGenre
+//   genre={"Action & Adventure"}
+//   isFetching = {this.props.isFetching}
+//   error = {this.props.error}
+//   movies = {this.props.movies}/>
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchActionAdventure();
+  }
+
+  render() {
+    const { actionAdventure } = this.props;
+
+    return (
+      <div>
+        <MainMovieContainer />
+        <TrendingContainer />
+        <MovieGenre
+          genre={'Action & Adventure'}
+          isFetching={actionAdventure.isFetching}
+          error={actionAdventure.error}
+          movies={actionAdventure.movies}
+        />
+        <ComediesContainer />
+        <DocumentariesContainer />
+        <DramasContainer />
+        <HorrorContainer />
+        <RomanceContainer />
+        <ScifiContainer />
+        <ThrillerContainer />
+      </div>
+    );
+  }
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return {
+    actionAdventure: state.actionAdventure
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchActionAdventure }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
