5b78efd0f0b88a2796b571d5c598f1fabab89e56
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-30 10:52:40 -0700

    Replace all genre containers with a reusable MovieGenre component. Remove all import statements regarding genre components inside Home container

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
 
 import MainMovieContainer from '../containers/MainMovieContainer';
-import ActionAdventureContainer from '../containers/ActionAdventureContainer';
-import ComediesContainer from '../containers/ComediesContainer';
-import DocumentariesContainer from '../containers/DocumentariesContainer';
-import DramasContainer from '../containers/DramasContainer';
-import HorrorContainer from '../containers/HorrorContainer';
-import RomanceContainer from '../containers/RomanceContainer';
-import ScifiContainer from '../containers/ScifiContainer';
-import ThrillerContainer from '../containers/ThrillerContainer';
-import TrendingContainer from '../containers/TrendingContainer';
 import MovieGenre from './MovieGenre/MovieGenre';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchActionAdventure } from '../actions';
-
-// <MovieGenre
-//   genre={"Action & Adventure"}
-//   isFetching = {this.props.isFetching}
-//   error = {this.props.error}
-//   movies = {this.props.movies}/>
+import { fetchComedies } from '../actions';
+import { fetchDocumentaries } from '../actions';
+import { fetchDramas } from '../actions';
+import { fetchHorror } from '../actions';
+import { fetchRomance } from '../actions';
+import { fetchScifi } from '../actions';
+import { fetchThriller } from '../actions';
+import { fetchTrending } from '../actions';
 
 class Home extends Component {
   constructor(props) {
@@ -28,29 +21,87 @@ class Home extends Component {
   }
 
   componentDidMount() {
+    this.props.fetchTrending();
     this.props.fetchActionAdventure();
+    this.props.fetchComedies();
+    this.props.fetchDocumentaries();
+    this.props.fetchDramas();
+    this.props.fetchHorror();
+    this.props.fetchRomance();
+    this.props.fetchScifi();
+    this.props.fetchThriller();
   }
 
   render() {
-    const { actionAdventure } = this.props;
+    const {
+      actionAdventure,
+      comedies,
+      documentaries,
+      dramas,
+      horror,
+      romance,
+      scifi,
+      thriller,
+      trending
+    } = this.props;
 
     return (
       <div>
         <MainMovieContainer />
-        <TrendingContainer />
+        <MovieGenre
+          genre={'Trending Now'}
+          isFetching={trending.isFetching}
+          error={trending.error}
+          movies={trending.movies}
+        />
         <MovieGenre
           genre={'Action & Adventure'}
           isFetching={actionAdventure.isFetching}
           error={actionAdventure.error}
           movies={actionAdventure.movies}
         />
-        <ComediesContainer />
-        <DocumentariesContainer />
-        <DramasContainer />
-        <HorrorContainer />
-        <RomanceContainer />
-        <ScifiContainer />
-        <ThrillerContainer />
+        <MovieGenre
+          genre={'Comedies'}
+          isFetching={comedies.isFetching}
+          error={comedies.error}
+          movies={comedies.movies}
+        />
+        <MovieGenre
+          genre={'Documentaries'}
+          isFetching={documentaries.isFetching}
+          error={documentaries.error}
+          movies={documentaries.movies}
+        />
+        <MovieGenre
+          genre={'Dramas'}
+          isFetching={dramas.isFetching}
+          error={dramas.error}
+          movies={dramas.movies}
+        />
+        <MovieGenre
+          genre={'Horror'}
+          isFetching={horror.isFetching}
+          error={horror.error}
+          movies={horror.movies}
+        />
+        <MovieGenre
+          genre={'Romance'}
+          isFetching={romance.isFetching}
+          error={romance.error}
+          movies={romance.movies}
+        />
+        <MovieGenre
+          genre={'Sci-Fi'}
+          isFetching={scifi.isFetching}
+          error={scifi.error}
+          movies={scifi.movies}
+        />
+        <MovieGenre
+          genre={'Thrillers'}
+          isFetching={thriller.isFetching}
+          error={thriller.error}
+          movies={thriller.movies}
+        />
       </div>
     );
   }
@@ -58,12 +109,33 @@ class Home extends Component {
 
 function mapStateToProps(state) {
   return {
-    actionAdventure: state.actionAdventure
+    trending: state.trending,
+    actionAdventure: state.actionAdventure,
+    comedies: state.comedies,
+    documentaries: state.documentaries,
+    dramas: state.dramas,
+    horror: state.horror,
+    romance: state.romance,
+    scifi: state.scifi,
+    thriller: state.thriller
   };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchActionAdventure }, dispatch);
+  return bindActionCreators(
+    {
+      fetchActionAdventure,
+      fetchComedies,
+      fetchDocumentaries,
+      fetchDramas,
+      fetchHorror,
+      fetchRomance,
+      fetchScifi,
+      fetchThriller,
+      fetchTrending
+    },
+    dispatch
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
