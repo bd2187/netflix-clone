1f04bd5b5f9a4aa7484746d1799a40c0f0f8ef1d
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-01 20:05:19 -0700

    Delete unused action creators in action directory

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,6 @@ import {
   searchEndpoint
 } from '../config/endpoints';
 
-// =========== Create reusable action creator for fetching genres
 export function fetchGenre(
   fetchingConstant,
   successConstant,
@@ -32,10 +31,6 @@ export function fetchGenre(
     }
   };
 }
-
-// fetchGenre
-
-// ===========
 
 export function fetchMainMovie() {
   return async function(dispatch) {
@@ -69,158 +64,6 @@ export function fetchTrending() {
     } catch (err) {
       dispatch({
         type: 'FETCHING_TRENDING_FAILURE',
-        error: err.toString()
-      });
-    }
-  };
-}
-
-export function fetchActionAdventure() {
-  return async function(dispatch) {
-    dispatch({ type: 'FETCHING_ACTION_ADVENTURE' });
-    try {
-      const response = await fetch(movieGenreEndpoint(28));
-      const data = await response.json();
-      dispatch({
-        type: 'FETCHING_ACTION_ADVENTURE_SUCCESS',
-        movies: data.results
-      });
-    } catch (err) {
-      dispatch({
-        type: 'FETCHING_ACTION_ADVENTURE_FAILURE',
-        error: err.toString()
-      });
-    }
-  };
-}
-
-export function fetchComedies() {
-  return async function(dispatch) {
-    dispatch({ type: 'FETCHING_COMEDIES' });
-    try {
-      const response = await fetch(movieGenreEndpoint(35));
-      const data = await response.json();
-      dispatch({
-        type: 'FETCHING_COMEDIES_SUCCESS',
-        movies: data.results
-      });
-    } catch (err) {
-      dispatch({
-        type: 'FETCHING_COMEDIES_FAILURE',
-        error: err.toString()
-      });
-    }
-  };
-}
-
-export function fetchDocumentaries() {
-  return async function(dispatch) {
-    dispatch({ type: 'FETCHING_DOCUMENTARIES' });
-    try {
-      const response = await fetch(movieGenreEndpoint(99));
-      const data = await response.json();
-      dispatch({
-        type: 'FETCHING_DOCUMENTARIES_SUCCESS',
-        movies: data.results
-      });
-    } catch (err) {
-      dispatch({
-        type: 'FETCHING_DOCUMENTARIES_FAILURE',
-        error: err.toString()
-      });
-    }
-  };
-}
-
-export function fetchDramas() {
-  return async function(dispatch) {
-    dispatch({ type: 'FETCHING_DRAMAS' });
-    try {
-      const response = await fetch(movieGenreEndpoint(18));
-      const data = await response.json();
-      dispatch({
-        type: 'FETCHING_DRAMAS_SUCCESS',
-        movies: data.results
-      });
-    } catch (err) {
-      dispatch({
-        type: 'FETCHING_DRAMAS_FAILURE',
-        error: err.toString()
-      });
-    }
-  };
-}
-
-export function fetchHorror() {
-  return async function(dispatch) {
-    dispatch({ type: 'FETCHING_HORROR' });
-    try {
-      const response = await fetch(movieGenreEndpoint(27));
-      const data = await response.json();
-      dispatch({
-        type: 'FETCHING_HORROR_SUCCESS',
-        movies: data.results
-      });
-    } catch (err) {
-      dispatch({
-        type: 'FETCHING_HORROR_FAILURE',
-        error: err.toString()
-      });
-    }
-  };
-}
-
-export function fetchRomance() {
-  return async function(dispatch) {
-    dispatch({ type: 'FETCHING_ROMANCE' });
-    try {
-      const response = await fetch(movieGenreEndpoint(10749));
-      const data = await response.json();
-      dispatch({
-        type: 'FETCHING_ROMANCE_SUCCESS',
-        movies: data.results
-      });
-    } catch (err) {
-      dispatch({
-        type: 'FETCHING_ROMANCE_FAILURE',
-        error: err.toString()
-      });
-    }
-  };
-}
-
-export function fetchScifi() {
-  return async function(dispatch) {
-    dispatch({ type: 'FETCHING_SCIFI' });
-    try {
-      const response = await fetch(movieGenreEndpoint(878));
-      const data = await response.json();
-      dispatch({
-        type: 'FETCHING_SCIFI_SUCCESS',
-        movies: data.results
-      });
-    } catch (err) {
-      dispatch({
-        type: 'FETCHING_SCIFI_FAILURE',
-        error: err.toString()
-      });
-    }
-  };
-}
-
-export function fetchThriller() {
-  return async function(dispatch) {
-    dispatch({ type: 'FETCHING_THRILLER' });
-    try {
-      const response = await fetch(movieGenreEndpoint(53));
-      const data = await response.json();
-      dispatch({
-        type: 'FETCHING_THRILLER_SUCCESS',
-        movies: data.results
-      });
-    } catch (err) {
-      dispatch({
-        type: 'FETCHING_THRILLER_FAILURE',
         error: err.toString()
       });
     }
