6d2aec1db1c12a6899b0057f3297cea52fbdaca1
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-02 11:50:28 -0700

    Replace all action type strings with action type constants inside of actions directory

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import * as constants from '../constants';
 import {
   mainMovieEndpoint,
   movieGenreEndpoint,
@@ -6,8 +7,6 @@ import {
   trailersEndpoint,
   searchEndpoint
 } from '../config/endpoints';
-
-// Todo: import constants and replace strings with them
 
 export function fetchGenre(
   fetchingConstant,
@@ -35,17 +34,17 @@ export function fetchGenre(
 
 export function fetchMainMovie() {
   return async function(dispatch) {
-    dispatch({ type: 'FETCHING_MOVIE' });
+    dispatch({ type: constants.FETCHING_MOVIE });
     try {
       const response = await fetch(mainMovieEndpoint);
       const data = await response.json();
       dispatch({
-        type: 'FETCHING_MAIN_MOVIE_SUCCESS',
+        type: constants.FETCHING_MAIN_MOVIE_SUCCESS,
         movie: data.results[0]
       });
     } catch (err) {
       dispatch({
-        type: 'FETCHING_MAIN_MOVIE_FAILURE',
+        type: constants.FETCHING_MAIN_MOVIE_FAILURE,
         error: err.toString()
       });
     }
@@ -54,17 +53,17 @@ export function fetchMainMovie() {
 
 export function fetchTrending() {
   return async function(dispatch) {
-    dispatch({ type: 'FETCHING_TRENDING' });
+    dispatch({ type: constants.FETCHING_TRENDING });
     try {
       const response = await fetch(trendingMoviesEndpoint);
       const data = await response.json();
       dispatch({
-        type: 'FETCHING_TRENDING_SUCCESS',
+        type: constants.FETCHING_TRENDING_SUCCESS,
         movies: data.results
       });
     } catch (err) {
       dispatch({
-        type: 'FETCHING_TRENDING_FAILURE',
+        type: constants.FETCHING_TRENDING_FAILURE,
         error: err.toString()
       });
     }
@@ -73,18 +72,18 @@ export function fetchTrending() {
 
 export function fetchSingleFilm(id) {
   return async function(dispatch) {
-    dispatch({ type: 'FETCHING_SINGLE_FILM' });
+    dispatch({ type: constants.FETCHING_SINGLE_FILM });
     try {
       const response = await fetch(movieDetailsEndpoint(id));
       var data = await response.json();
       dispatch({
-        type: 'FETCHING_SINGLE_FILM_SUCCESS',
+        type: constants.FETCHING_SINGLE_FILM_SUCCESS,
         film: data
       });
       dispatch(fetchTrailers(id));
     } catch (err) {
       dispatch({
-        type: 'FETCHING_SINGLE_FILM_FAILURE',
+        type: constants.FETCHING_SINGLE_FILM_FAILURE,
         error: err.toString()
       });
     }
@@ -93,18 +92,18 @@ export function fetchSingleFilm(id) {
 
 function fetchTrailers(id) {
   return async function(dispatch) {
-    dispatch({ type: 'FETCHING_TRAILERS' });
+    dispatch({ type: constants.FETCHING_TRAILERS });
 
     try {
       const response = await fetch(trailersEndpoint(id));
       const data = await response.json();
       dispatch({
-        type: 'FETCHING_TRAILERS_SUCCESS',
+        type: constants.FETCHING_TRAILERS_SUCCESS,
         trailers: [data.results[0], data.results[1]]
       });
     } catch (err) {
       dispatch({
-        type: 'FETCHING_TRAILER_FAILURE',
+        type: constants.FETCHING_TRAILER_FAILURE,
         error: err.toString()
       });
     }
@@ -113,19 +112,19 @@ function fetchTrailers(id) {
 
 export function searchFilm(query) {
   return async function(dispatch) {
-    dispatch({ type: 'FETCHING_SEARCH', query });
+    dispatch({ type: constants.FETCHING_SEARCH, query });
 
     try {
       const response = await fetch(searchEndpoint(query));
       const data = await response.json();
       dispatch({
-        type: 'FETCHING_SEARCH_SUCCESS',
+        type: constants.FETCHING_SEARCH_SUCCESS,
         query,
         movies: data.results
       });
     } catch (err) {
       dispatch({
-        type: 'FETCHING_SEARCH_FAILURE',
+        type: constants.FETCHING_SEARCH_FAILURE,
         query,
         error: err.toString()
       });
